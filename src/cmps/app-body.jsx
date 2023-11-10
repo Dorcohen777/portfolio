1553ddaa737d794fee0ef645f70efd3f06a1f762
Dor cohen <78268224+plutus777@users.noreply.github.com>
2023-11-10 14:29:36 +0200

    added custom cursor to body section

--- src/cmps/app-body.jsx
+++ src/cmps/app-body.jsx
@@ -2,55 +2,106 @@
 import heroImg from '../assets/img/me.jpeg'
 import linkedinImg from '../assets/img/linkedin.png'
 import gitHub from '../assets/img/github.png'
-import shape from '../assets/img/noun-polka-dots-5067076.svg'
 import arrSvg from '../assets/img/arrsvg.svg'
 import cv from '../assets/cv.docx'
 import { motion } from 'framer-motion'
+import { useEffect, useState } from 'react'
 
 export function AppBody() {
+   const [mousePosition, setMousePosition] = useState({
+      x: 0,
+      y: 0,
+   })
+   const [cursorVariant, setCursorVariant] = useState('default')
+
+   useEffect(() => {
+      const mouseMove = (e) => {
+         setMousePosition({
+            x: e.clientX,
+            y: e.clientY,
+         })
+      }
+
+      window.addEventListener('mousemove', mouseMove)
+
+      return () => {
+         window.removeEventListener('mousemove', mouseMove)
+      }
+   }, [])
+
+   const variants = {
+      default: {
+         x: mousePosition.x - 16,
+         y: mousePosition.y - 16,
+      },
+      text: {
+         height: 150,
+         width: 150,
+         x: mousePosition.x - 75,
+         y: mousePosition.y - 75,
+         backgroundColor: '#fff',
+         mixBlendMode: 'difference',
+      },
+      buttons: {
+         display: 'none',
+         x: mousePosition.x - 16,
+         y: mousePosition.y - 16,
+      },
+   }
+
+   const textEnter = () => setCursorVariant('text')
+   const textLeave = () => setCursorVariant('default')
+   const onButtons = () => setCursorVariant('buttons')
+   const onLeaveButtons = () => setCursorVariant('default')
    return (
       <section className='hero-container' id='about'>
+         <motion.div
+            className='cursor'
+            variants={variants}
+            animate={cursorVariant}
+         />
          <div className='hero-section'>
-            <motion.div
-               initial={{ opacity: 0 }}
-               animate={{ opacity: 1, transition: { delay: 2.5 } }}
-            >
-               {/* <img src={shape} alt='shape' className='hero-shape' /> */}
-            </motion.div>
-            <motion.h1
-               className='animate__animated animate__fadeInDown'
-               initial={{ opacity: 0, x: -80 }}
-               animate={{ opacity: 1, x: 0, transition: { delay: 0.3 } }}
-            >
-               Hey, i'm Dor.
-            </motion.h1>
+            <div onMouseEnter={textEnter} onMouseLeave={textLeave}>
+               <motion.h1
+                  className='animate__animated animate__fadeInDown'
+                  initial={{ opacity: 0, x: -80 }}
+                  animate={{ opacity: 1, x: 0, transition: { delay: 0.3 } }}
+               >
+                  Hey, i'm Dor.
+               </motion.h1>
 
-            <motion.h2
-               initial={{ opacity: 0, x: -80 }}
-               animate={{ opacity: 1, x: 0, transition: { delay: 0.6 } }}
-            >
-               Full-Stack Web Developer
-            </motion.h2>
+               <motion.h2
+                  initial={{ opacity: 0, x: -80 }}
+                  animate={{ opacity: 1, x: 0, transition: { delay: 0.6 } }}
+               >
+                  Full-Stack Web Developer
+               </motion.h2>
 
-            <motion.p
-               initial={{ opacity: 0, y: 50 }}
-               animate={{ opacity: 1, y: 0, transition: { delay: 0.9 } }}
-            >
-               I specialize in website development with advanced technologies
-               and programming languages, digital marketing, user experience and
-               more...
-            </motion.p>
-            <br />
-            <motion.p
-               initial={{ opacity: 0, y: 100 }}
-               animate={{ opacity: 1, y: 0, transition: { delay: 1.2 } }}
-            >
-               Collaborative team player with excellent technical abilities,
-               emphasis on the small details, focusing on problem solving.
-               self-learner and adapt quickly to new environments.
-            </motion.p>
+               <div className='hero-text-container'>
+                  <motion.p
+                     initial={{ opacity: 0, y: 50 }}
+                     animate={{ opacity: 1, y: 0, transition: { delay: 0.9 } }}
+                  >
+                     I specialize in website development with advanced
+                     technologies and programming languages, digital marketing,
+                     user experience and more...
+                  </motion.p>
+                  <br />
+                  <motion.p
+                     initial={{ opacity: 0, y: 100 }}
+                     animate={{ opacity: 1, y: 0, transition: { delay: 1.2 } }}
+                  >
+                     Collaborative team player with excellent technical
+                     abilities, emphasis on the small details, focusing on
+                     problem solving. self-learner and adapt quickly to new
+                     environments.
+                  </motion.p>
+               </div>
+            </div>
             <motion.div
                className='hero-actions-container'
+               onMouseEnter={onButtons}
+               onMouseLeave={onLeaveButtons}
                initial={{ opacity: 0, y: 20 }}
                animate={{ opacity: 1, y: 0, transition: { delay: 1.5 } }}
             >
@@ -91,7 +142,6 @@ export function AppBody() {
             animate={{ opacity: 1, x: 0, transition: { delay: 2.5 } }}
          >
             <img src={heroImg} alt='hero-img' className='hero-img' />
-            <div className='hero-border'></div>
          </motion.div>
          <motion.div
             initial={{ opacity: 0 }}
